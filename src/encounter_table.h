1fdfa7942f463c0e4f191451d3c0f4112ff4e964
Ben <109313313+10BenAgain@users.noreply.github.com>
2024-10-18 15:57:40 -0600

    Added encounter slot lookup tables

---
+++ src/encounter_table.h
@@ -0,0 +1,104 @@
+#ifndef ENCOUNTER_H
+#define ENCOUNTER_H
+
+#include <stdint.h>
+
+typedef enum {
+    Grass,
+    Water,
+    RockSmash,
+    OldRod,
+    GoodRod,
+    SuperRod
+} EncounterType;
+
+static const uint8_t
+grass_encounter_slot_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+        6, 6, 6, 6, 6,
+        7, 7, 7, 7, 7,
+        8, 8, 8, 8,
+        9, 9, 9, 9,
+        10,
+        11
+};
+
+static const uint8_t
+water_encounter_slot_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2,
+        3, 3, 3, 3,
+        4
+};
+
+static const uint8_t
+rock_smash_encounter_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2,
+        3, 3, 3, 3,
+        4
+};
+
+static const uint8_t
+old_rod_encounter_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+};
+
+static const uint8_t
+good_rod_encounter_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+};
+
+static const uint8_t
+super_rod_encounter_lookup_table[100] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3,
+        4
+};
+
+static const uint8_t
+*get_enc_table(EncounterType enc) {
+    switch (enc) {
+        case Grass:
+            return grass_encounter_slot_lookup_table;
+        case Water:
+            return water_encounter_slot_lookup_table;
+        case RockSmash:
+            return rock_smash_encounter_lookup_table;
+        case OldRod:
+            return old_rod_encounter_lookup_table;
+        case GoodRod:
+            return good_rod_encounter_lookup_table;
+        case SuperRod:
+            return super_rod_encounter_lookup_table;
+        default:
+            return grass_encounter_slot_lookup_table;
+    }
+}
+
+#endif
