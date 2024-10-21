c8a131c179503cc75afe9cc5cd302f405e662e42
Ben <109313313+10BenAgain@users.noreply.github.com>
2024-10-21 16:22:26 -0600

    Add jump table logic

--- src/rng.c
+++ src/rng.c
@@ -218,6 +218,53 @@ nextUShort(uint16_t max, uint32_t seed) {
     return rand % max;
 }
 
+/*
+    *  jump[1].add = 0x6073 * 0x41C64E6D + 1 = 0x6073 * 0x41C64E6E = 0x18C7E97E7B6A & 0xFFFFFFF (uint32_t cast) = 0xE97E7B6A
+    *  jump[1].multiplier = 0x41C64E6D * 0x41C64E6D = 0x10E64D70C2A29A69 & 0xFFFFFFF = 0xC2A29A69
+    *
+    *  Jump[1] Add = 0xE97E7B6A
+    *  Jump[1] Multiplier = 0xC2A29A69
+    *
+    *  Jump[2].add = Jump[1] Add * (Jump[1] Multiplier + 1) = 0xE97E7B6A * 0xC2A29A6A = 0xB186286731B0DDE4 & 0xFFFFFFFF = 0x31B0DDE4
+    *  Jump[2].multiplier = Jump[1] Multiplier * Jump[1] Multiplier = 0x93FAD94EEE067F11 & 0xFFFFFFFF = 0xEE067F11
+    *
+    *  Jump[2] Add = 0x31B0DDE4
+    *  Jump[2] Multiplier = 0xEE067F11
+    *
+    *  Etc..
+*/
+
+// https://github.com/Admiral-Fish/PokeFinder/blob/a0c55aec852618a4afff3116b48b3e7df7ba07df/Source/Core/RNG/LCRNG.cpp#L20-L36
+JumpTable
+*compute_jump_table() {
+    JumpTable *jt = malloc(sizeof(JumpTable));
+    jt->jump[0].add = INCREMENT;
+    jt->jump[0].multiplier = MULTIPLIER;
+
+    for ( size_t i = 1; i < 32; i++) {
+        jt->jump[i].add = jt->jump[i - 1].add * (jt->jump[i - 1].multiplier + 1);
+        jt->jump[i].multiplier = jt->jump[i - 1].multiplier * jt->jump[i - 1].multiplier;
+    }
+    return jt;
+}
+
+/*
+* Seed = 0xEE53, i = 0, Advances = 10 | Advances & 1 = 0 | Then 10 >>= 1 = 8, etc..
+* Once i = 3, then (advances & 1) is True
+* Then seed = ( 0xEE53 * 0xCFDDDF21 + 0x67DBB608 ) = 0xD542F91D
+*/
+uint32_t
+jump_ahead(const Jump table[32], uint32_t seed, uint32_t advances) {
+    int i;
+    for (i = 0; advances; advances >>= 1, i++) {
+        printf("%d - %d\n", i, advances);
+        if (advances & 1) {
+            seed = seed * table[i].multiplier + table[i].add;
+        }
+    }
+    return seed;
+}
+
 // Probably a very roundabout way of calculating the inverse C
 void 
 check_seed(uint32_t *seed) {
--- src/rng.h
+++ src/rng.h
@@ -1,6 +1,7 @@
 #ifndef RNG_H
 #define RNG_H
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "encounter_table.h"
@@ -23,6 +24,58 @@
 #define CINVERSE    0xA3561A1
 #define IV_MASK     0x1F
 #define IV_SHIFT    5
+
+typedef struct {
+    uint32_t multiplier;
+    uint32_t add;
+} Jump;
+
+typedef struct {
+    Jump jump[32];
+} JumpTable;
+
+/*Result from generating jump table with gen 3 LCRNG values (0x6073 , 0x41C64E6D) */
+static const Jump Gen3JumpTable[32] = {
+        { 0x41C64E6D , 0x6073     },
+        { 0xC2A29A69 , 0xE97E7B6A },
+        { 0xEE067F11 , 0x31B0DDE4 },
+        { 0xCFDDDF21 , 0x67DBB608 },
+        { 0x5F748241 , 0xCBA72510 },
+        { 0x8B2E1481 , 0x1D29AE20 },
+        { 0x76006901 , 0xBA84EC40 },
+        { 0x1711D201 , 0x79F01880 },
+        { 0xBE67A401 , 0x8793100  },
+        { 0xDDDF4801 , 0x6B566200 },
+        { 0x3FFE9001 , 0x803CC400 },
+        { 0x90FD2001 , 0xA6B98800 },
+        { 0x65FA4001 , 0xE6731000 },
+        { 0xDBF48001 , 0x30E62000 },
+        { 0xF7E90001 , 0xF1CC4000 },
+        { 0xEFD20001 , 0x23988000 },
+        { 0xDFA40001 , 0x47310000 },
+        { 0xBF480001 , 0x8E620000 },
+        { 0x7E900001 , 0x1CC40000 },
+        { 0xFD200001 , 0x39880000 },
+        { 0xFA400001 , 0x73100000 },
+        { 0xF4800001 , 0xE6200000 },
+        { 0xE9000001 , 0xCC400000 },
+        { 0xD2000001 , 0x98800000 },
+        { 0xA4000001 , 0x31000000 },
+        { 0x48000001 , 0x62000000 },
+        { 0x90000001 , 0xC4000000 },
+        { 0x20000001 , 0x88000000 },
+        { 0x40000001 , 0x10000000 },
+        { 0x80000001 , 0x20000000 },
+        { 0x1 , 0x40000000        },
+        { 0x1 , 0x80000000        }
+};
+
+/*
+ * Never used except to generate the above table
+ * There is no need to generate the table multiple times
+*/
+JumpTable *compute_jump_table();
+uint32_t jump_ahead(const Jump table[32], uint32_t seed, uint32_t advances);
 
 void increment_seed(uint32_t *seed, uint32_t advances);
 void decrement_seed(uint32_t *seed, uint32_t advances);
